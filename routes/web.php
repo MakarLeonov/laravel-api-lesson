3d6876f7d8516069220b95687da142e29fbc274c
unknown <MakarLeonov>
2023-04-06 01:48:59 +0300

    Added api-controller whith index and show methods

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Api\DeskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/desks', [DeskController::class, 'index']);
+
+Route::apiResources([
+    'desks' => DeskController::class,
+]);
